117fb594951e03bc8f88848941723e2d4fb33f74
Sayandeep <sarkarsayandeep093@gmail.com>
2025-08-21 11:20:51 +0530

    first commit

---
+++ src/main/java/net/javaguides/springboot_app/WelcomeController.java
@@ -0,0 +1,26 @@
+package net.javaguides.springboot_app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+
+    // Home Page
+    @GetMapping("/")
+    public String home() {
+        return "index";   // loads index.html from templates
+    }
+
+    // Welcome Page
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "welcome"; // loads welcome.html
+    }
+
+    // Hello Page
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";   // loads hello.html
+    }
+}
